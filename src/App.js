1a20d513c94b0657d2cbbe77bb29e8eaf0fd5a41
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-15 14:54:24 -0600

    Created the Axios functions to display content from API.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  getAdoptablePets = () => {
+
+    axios({
+      // We can configure everything we need to about the http request in here
+      method: 'POST',
+      url: 'https://api.petfinder.com/v2/oauth2/token',
+      data: {
+        grant_type: 'client_credentials',
+        client_id: 'uTs1oyTZOCH08ubX11sIAPPDN9XFYRoJ0hYlML5fofyMt0RuiT',
+        client_secret: '0ijkQV9UXKMTja7iOScbNXsG5fxfU8GPWEDaLYgj'
+      }
+      }).then(function (response) {
+        
+        axios({
+          // We can configure everything we need to about the http request in here
+          method: 'GET',
+          url: 'https://api.petfinder.com/v2/animals?type=dog&page=2',
+          headers: { Authorization: `Bearer ${response.data.access_token}` }
+          }).then(function (response) {
+            console.log(response);
+          }).catch(function (error) {
+              console.log(error);
+          });
+          
+      }).catch(function (error) {
+          console.log(error);
+      })
+  }
+
+
+  render() {
+
+    this.getAdoptablePets();
+    return (
+
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;

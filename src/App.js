ca4e8b7bed221504c16462db8dd7b03026b6599b
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-17 22:52:01 -0600

    Created Footer Component and styled it.  Imported Footer to App.js and called it in the render function.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
+import Footer from './Components/Footer/Footer';
 
 class App extends React.Component {
 
@@ -11,6 +12,7 @@ class App extends React.Component {
       <div className="App">
         <Header />
         <Main />
+        <Footer />
       </div>
     );
   }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Footer.css';
+
+export default class Footer extends Component {
+    render() {
+        return(
+            <footer className='Footer'>
+                <h1 className='Footer-logo'>
+                    <a className='Footer-logoLink' href='#'>AdoptMe</a>
+                </h1> 
+                {/* ^ Link to return to top */}
+            </footer>
+        );
+    }
+}

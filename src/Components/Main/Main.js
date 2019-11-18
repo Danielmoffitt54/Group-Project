dfdf934f3112064f45f4c20b069b6181f0f01f40
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-17 21:19:10 -0600

    Changed MainContent component to List component.  Created List component with a JS and CSS file.  Created page navigator on Main component.

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import './Main.css';
-// import Filters from './Components/Filters/Filters';
-// import MainContent from './Components/MainContent/MainContent';
+import List from '../List/List';
 
 export default class Main extends Component {
     render() {
         return(
             <main className='Main'>
                 {/* <Filters /> */}
-                {/* <MainContent /> */}
+                <List />
+                <div className='Main-pages'>
+                    <div className='Main-pageNav'>
+                        <button onClick={console.log('hi')} className='Main-navBtn'>Prev</button>
+                        <div className='Main-navMid'>#</div>
+                        <button onClick={console.log('hi')} className='Main-navBtn'>Next</button>
+                    </div>
+                </div>
             </main>
         );
     }

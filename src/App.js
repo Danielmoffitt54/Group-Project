3305b2d85d08c03a47228060e73d17c60c4b00f9
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-17 22:01:27 -0600

    Moved Axios call to Main Component.  Created states pages and animals to be updated when the get request comes through.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,11 @@
 import React from 'react';
-import axios from 'axios';
 import Header from './Components/Header/Header';
 import Main from './Components/Main/Main';
 
 class App extends React.Component {
 
-  getAdoptablePets = () => {
-
-    axios({
-      // We can configure everything we need to about the http request in here
-      method: 'POST',
-      url: 'https://api.petfinder.com/v2/oauth2/token',
-      data: {
-        grant_type: 'client_credentials',
-        client_id: 'uTs1oyTZOCH08ubX11sIAPPDN9XFYRoJ0hYlML5fofyMt0RuiT',
-        client_secret: '0ijkQV9UXKMTja7iOScbNXsG5fxfU8GPWEDaLYgj'
-      }
-      }).then(function (response) {
-        
-        axios({
-          // We can configure everything we need to about the http request in here
-          method: 'GET',
-          url: 'https://api.petfinder.com/v2/animals?type=dog&page=2',
-          headers: { Authorization: `Bearer ${response.data.access_token}` }
-          }).then(function (response) {
-            console.log(response);
-          }).catch(function (error) {
-              console.log(error);
-          });
-          
-      }).catch(function (error) {
-          console.log(error);
-      })
-  }
-
-
   render() {
 
-    this.getAdoptablePets();
     return (
 
       <div className="App">
--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,18 +1,69 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './Main.css';
 import List from '../List/List';
 
 export default class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            animals: [],
+            page: 0,
+        }
+    }
+
+    getAdoptablePets = () => {
+
+        const self = this;
+  
+      axios({
+        // We can configure everything we need to about the http request in here
+        method: 'POST',
+        url: 'https://api.petfinder.com/v2/oauth2/token',
+        data: {
+          grant_type: 'client_credentials',
+          client_id: 'uTs1oyTZOCH08ubX11sIAPPDN9XFYRoJ0hYlML5fofyMt0RuiT',
+          client_secret: '0ijkQV9UXKMTja7iOScbNXsG5fxfU8GPWEDaLYgj'
+        }
+        }).then(function (response) {
+          
+          axios({
+            // We can configure everything we need to about the http request in here
+            method: 'GET',
+            url: 'https://api.petfinder.com/v2/animals',
+            headers: { Authorization: `Bearer ${response.data.access_token}` }
+            }).then(function (response) {
+                console.log(response);
+                self.setState({
+                    animals: response.data.animals,
+                    page: response.data.pagination.current_page
+                });
+            }).catch(function (error) {
+                console.log(error);
+            });
+            
+        }).catch(function (error) {
+            console.log(error);
+        })
+    }
+
+    componentDidMount() {
+        this.getAdoptablePets();
+    }
+
+
     render() {
+        const { page, animals } = this.state;
+
         return(
             <main className='Main'>
                 {/* <Filters /> */}
-                <List />
+                <List array={animals}/>
                 <div className='Main-pages'>
                     <div className='Main-pageNav'>
-                        <button onClick={console.log('hi')} className='Main-navBtn'>Prev</button>
-                        <div className='Main-navMid'>#</div>
-                        <button onClick={console.log('hi')} className='Main-navBtn'>Next</button>
+                        <button className='Main-navBtn'>Prev</button>
+                        <div className='Main-navMid'>{page}</div>
+                        <button className='Main-navBtn'>Next</button>
                     </div>
                 </div>
             </main>

2d3f18a1429d374189f67d8b8b27ffce046f5d05
Daniel Moffitt <Danielmoffitt54@gmail.com>
2019-11-18 18:24:01 -0600

    Created functions incrementPage and decrement page to navigate between pages. Added border to the top of the Footer Component.

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -8,14 +8,40 @@ export default class Main extends Component {
         super(props);
         this.state = {
             animals: [],
-            page: 0,
+            currentPage: 1,
+            totalPages: ''
+        }
+        this.incrementPage = this.incrementPage.bind(this);
+        this.decrementPage = this.decrementPage.bind(this);
+    }
+
+    incrementPage() {
+        const { currentPage, totalPages } = this.state;
+        const lastPage = currentPage === totalPages;
+        if (!lastPage) {
+            this.setState({
+                currentPage: currentPage + 1
+            })
+            return this.getAdoptablePets()
+        }
+    }
+
+    decrementPage() {
+        const { currentPage } = this.state;
+        const firstPage = currentPage === 1;
+        if (!firstPage) {
+            this.setState({
+                currentPage: currentPage - 1
+            })
+            return this.getAdoptablePets()
         }
     }
 
     getAdoptablePets = () => {
 
         const self = this;
-  
+        const { currentPage } = this.state;
+
       axios({
         // We can configure everything we need to about the http request in here
         method: 'POST',
@@ -30,13 +56,13 @@ export default class Main extends Component {
           axios({
             // We can configure everything we need to about the http request in here
             method: 'GET',
-            url: 'https://api.petfinder.com/v2/animals',
+            url: `https://api.petfinder.com/v2/animals?page=${currentPage}`,
             headers: { Authorization: `Bearer ${response.data.access_token}` }
             }).then(function (response) {
                 console.log(response);
                 self.setState({
                     animals: response.data.animals,
-                    page: response.data.pagination.current_page
+                    totalPages: response.data.pagination.total_pages
                 });
             }).catch(function (error) {
                 console.log(error);
@@ -51,19 +77,17 @@ export default class Main extends Component {
         this.getAdoptablePets();
     }
 
-
     render() {
-        const { page, animals } = this.state;
+        const { currentPage, animals } = this.state;
 
         return(
             <main className='Main'>
-                {/* <Filters /> */}
                 <List array={animals}/>
                 <div className='Main-pages'>
                     <div className='Main-pageNav'>
-                        <button className='Main-navBtn'>Prev</button>
-                        <div className='Main-navMid'>{page}</div>
-                        <button className='Main-navBtn'>Next</button>
+                        <button className='Main-navBtn' onClick={this.decrementPage}>Prev</button>
+                        <div className='Main-navMid'>{currentPage}</div>
+                        <button className='Main-navBtn' onClick={this.incrementPage}>Next</button>
                     </div>
                 </div>
             </main>
